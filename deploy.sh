6dbea4173640886b64cfbb2e0b09475c9c2d1231
Luis Martins <luismwolf@gmail.com>
2024-02-19 10:57:12 -0500

    added continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! bin/bash
+sudo apt update && sudo apt install nodejs npm
+# Install pm2
+sudo npm install -g pm2
+# Stop any instance of our application running currently
+pm2 stop example_app
+# Change directory to app's directory
+cd devops-lab-3-task/
+# Install app dependencies
+npm install
+# Start application on pm2
+pm2 start ./bin/www --name example_app
