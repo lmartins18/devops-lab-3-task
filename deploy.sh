0808785a37aa96f9b8b74e1967256b138ead60b2
Luis Martins <luismwolf@gmail.com>
2024-03-15 12:36:27 -0400

    lab9

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,35 @@
-#! bin/bash
-sudo apt -y update && sudo apt -y install nodejs npm
-# Install pm2
-sudo npm install -g pm2
-# Stop any instance of our application running currently
-pm2 stop example_app
-# Change directory to app's directory
-cd devops-lab-3-task/
-# Install app dependencies
-npm install
+#!/usr/bin/env bash
+# Check if there is instance running with the image name we are deploying
+CURRENT_INSTANCE=$(docker ps -a -q -—filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
+# If an instance does exist stop the instance
+if [ "$CURRENT_INSTANCE" ] 
+then
+    docker rm $(docker stop $CURRENT_INSTANCE)
+fi
+
+# Pull down the instance from dockerhub
+docker pull $IMAGE_NAME
+
+# Check if a docker container exists with the name of node_app if it does remove the container
+CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+if [ "$CONTAINER_EXISTS" ] 
+then
+    docker rm node_app
+fi
+
+# Create a container called node_app that is available on port 8443 from our docker image
+docker create -p 8443:8443 --name node_app $IMAGE_NAME
+
+# Write the private key to a file
 echo $PRIVATE_KEY > privatekey.pem
+# Write the server key to a file
 echo $SERVER > server.crt
-# Start application on pm2
-pm2 start ./bin/www --name example_app
+
+# Add the private key to the node_app docker container
+docker cp ./privatekey.pem node_app:/privatekey.pem
+
+# Add the server key to the node_app docker container
+docker cp ./server.crt node_app:/server.crt
+
+# start the node_app container
+docker start node_app
